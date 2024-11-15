fd5d95c8ce0becfa517a11132ea3a881b244583a
a24noags <a24noags@iessanclemente.net>
2024-11-15 12:51:15 +0100

    Carpetas static y a mitad de blog/details

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Blogs
 
 # Create your views here.
@@ -7,3 +7,7 @@ def all_blogs(request):
     # Selects all the blogs, and returns them in descending order
     all_blogs = Blogs.objects.all().order_by('-date')
     return render(request, 'blog/all_blogs.html', {"blogs": all_blogs})
+
+def details(request, blog_id):
+    blog = get_object_or_404(Blogs, pk=blog_id)
+    return render(request, 'blog/details.html', {'blog' : blog})
