484c5abc4ed09218b2b7d2a681f92392d2a657f7
a24noags <a24noags@iessanclemente.net>
2024-12-04 10:11:17 +0100

    Ajustes para poder trabjar en local

--- portfolioPro/settings.py
+++ portfolioPro/settings.py
@@ -125,6 +125,11 @@ STATIC_ROOT = BASE_DIR / 'static'
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media/'
 
+try:
+    from .local_setings import *
+except ImportError:
+    print("No local file")
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
